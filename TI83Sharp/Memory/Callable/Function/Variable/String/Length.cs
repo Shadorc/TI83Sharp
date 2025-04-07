7784f19fee1bf89e360c3dfda7073d126aba9863
Shadorc <d.ceccarelli@outlook.fr>
2025-04-08 00:23:22 +0200

    Add basic implementation of length( command

---
+++ TI83Sharp/Memory/Callable/Function/Variable/String/Length.cs
@@ -0,0 +1,22 @@
+﻿
+namespace TI83Sharp;
+
+public class Length : Function
+{
+    public Length() : base("length(", 1)
+    {
+    }
+
+    public override object Call(Interpreter interpreter, List<Expr> arguments)
+    {
+        var arg = interpreter.Evaluate(arguments[0]);
+        if (arg is not string str)
+        {
+            throw RuntimeError.DataType;
+        }
+
+        // TODO: The length is measured in the number of tokens, and not the number of letters in the string
+
+        return (TiNumber)str.Length;
+    }
+}
---
+++ TI83SharpTests/MemoryTests/CallableTests/FunctionTests/Variable/StringTests/LengthTests.cs
@@ -0,0 +1,51 @@
+﻿using TI83Sharp;
+using static TI83SharpTests.TestsHelper;
+
+namespace TI83SharpTests;
+
+[TestClass]
+public class LengthTests
+{
+    [TestMethod]
+    public void TestLength()
+    {
+        var source = @"
+            :""Hello""→Str1
+            :length(Str1)→B
+            ";
+        Interpret(source, out _, out var environment);
+        Assert.AreEqual((TiNumber)5, environment.Get<TiNumber>("B"));
+    }
+
+    [TestMethod]
+    public void TestLength_EmptyString()
+    {
+        var source = @"
+            :""→Str1
+            :length(Str1)→B
+            ";
+        Interpret(source, out _, out var environment);
+        Assert.AreEqual((TiNumber)0, environment.Get<TiNumber>("B"));
+    }
+
+    [TestMethod]
+    public void TestLength_NonString()
+    {
+        var source = @"
+            :1→A
+            :length(A)→B
+            ";
+        AssertInterpretThrows<RuntimeError>(source, "ERR:DATA TYPE");
+    }
+
+    [TestMethod]
+    public void TestLength_Token()
+    {
+        var source = @"
+            :""sin(""→Str1
+            :length(Str1)→B
+            ";
+        Interpret(source, out _, out var environment);
+        Assert.AreEqual((TiNumber)1, environment.Get<TiNumber>("B"));
+    }
+}
