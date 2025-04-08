6d03de3fb92fd748d4d7f2f83223429a50c5edda
Shadorc <d.ceccarelli@outlook.fr>
2025-04-09 00:37:24 +0200

    Add sub( command

---
+++ TI83Sharp/Memory/Callable/Function/Variable/String/Sub.cs
@@ -0,0 +1,80 @@
+﻿namespace TI83Sharp;
+
+public class Sub : Function
+{
+    public Sub() : base("sub(", 1, 3)
+    {
+    }
+
+    public override object Call(Interpreter interpreter, List<Expr> arguments)
+    {
+        var arg1 = arguments[0];
+        if (arg1 is Variable variable && variable.Name.Type == TokenType.StringId)
+        {
+            var name = variable.Name.Lexeme;
+            if (!interpreter.Environment.Exists(name))
+            {
+                throw RuntimeError.Undefined;
+            }
+
+            return ComputeString(interpreter, arguments, interpreter.Environment.Get<string>(name));
+        }
+
+        var evaluatedArg = interpreter.Evaluate(arg1);
+
+        if (evaluatedArg is string str)
+        {
+            return ComputeString(interpreter, arguments, str);
+        }
+
+        if (arguments.Count != 1)
+        {
+            throw RuntimeError.Argument;
+        }
+
+        if (evaluatedArg is TiNumber number)
+        {
+            return ComputeNumber(number);
+        }
+        else if (evaluatedArg is TiList list)
+        {
+            var result = new TiList(list.Count);
+            foreach (var item in list)
+            {
+                result.Add(ComputeNumber(item));
+            }
+            return result;
+        }
+
+        throw RuntimeError.DataType;
+    }
+
+    private static string ComputeString(Interpreter interpreter, List<Expr> arguments, string str)
+    {
+        if (arguments.Count != 3)
+        {
+            throw RuntimeError.Argument;
+        }
+
+        var arg2 = interpreter.Evaluate(arguments[1]);
+        var arg3 = interpreter.Evaluate(arguments[2]);
+
+        if (arg2 is not TiNumber start || !start.IsInt() || start < 1
+            || arg3 is not TiNumber length || !length.IsInt() || length < 1)
+        {
+            throw RuntimeError.Domain;
+        }
+
+        if (start + length > str.Length + 1)
+        {
+            throw RuntimeError.InvalidDim;
+        }
+
+        return str.Substring(start - 1, length);
+    }
+
+    private static TiNumber ComputeNumber(TiNumber number)
+    {
+        return number / (TiNumber)100;
+    }
+}
---
+++ TI83SharpTests/MemoryTests/CallableTests/FunctionTests/Variable/StringTests/SubTests.cs
@@ -0,0 +1,129 @@
+﻿using TI83Sharp;
+using static TI83SharpTests.TestsHelper;
+
+namespace TI83SharpTests;
+
+[TestClass]
+public class SubTests
+{
+    [TestMethod]
+    public void TestSub_String()
+    {
+        var source = @"
+            :""Hello""→Str1
+            :sub(Str1,3,3)→Str2
+            ";
+        Interpret(source, out _, out var environment);
+        Assert.AreEqual("llo", environment.Get<string>("Str2"));
+    }
+
+    [TestMethod]
+    public void TestSub_StringConstant()
+    {
+        var source = @"
+            :sub(""Hello"",3,3)→Str2
+            ";
+        Interpret(source, out _, out var environment);
+        Assert.AreEqual("llo", environment.Get<string>("Str2"));
+    }
+
+    [TestMethod]
+    public void TestSub_String_IndexOf()
+    {
+        var source = @"
+            :""Hello""→Str1
+            :sub(Str1,2,1)→Str2
+            ";
+        Interpret(source, out _, out var environment);
+        Assert.AreEqual("e", environment.Get<string>("Str2"));
+    }
+
+    [TestMethod]
+    public void TestSub_String_ErrNegativeIndex()
+    {
+        var source = @"
+            :""Hello""→Str1
+            :sub(Str1,-2,2)→Str2
+            ";
+        AssertInterpretThrows<RuntimeError>(source, "ERR:DOMAIN");
+    }
+
+    [TestMethod]
+    public void TestSub_String_ErrNegativeLength()
+    {
+        var source = @"
+            :""Hello""→Str1
+            :sub(Str1,2,-2)→Str2
+            ";
+        AssertInterpretThrows<RuntimeError>(source, "ERR:DOMAIN");
+    }
+
+    [TestMethod]
+    public void TestSub_String_ErrZeroIndex()
+    {
+        var source = @"
+            :""Hello""→Str1
+            :sub(Str1,0,1)→Str2
+            ";
+        AssertInterpretThrows<RuntimeError>(source, "ERR:DOMAIN");
+    }
+
+    [TestMethod]
+    public void TestSub_String_ErrInvalidDimStart()
+    {
+        var source = @"
+            :""Hello""→Str1
+            :sub(Str1,6,1)→Str2
+            ";
+        AssertInterpretThrows<RuntimeError>(source, "ERR:INVALID DIM");
+    }
+
+    [TestMethod]
+    public void TestSub_String_ErrInvalidDimLength()
+    {
+        var source = @"
+            :""Hello""→Str1
+            :sub(Str1,2,5)→Str2
+            ";
+        AssertInterpretThrows<RuntimeError>(source, "ERR:INVALID DIM");
+    }
+
+    [TestMethod]
+    public void TestSub_String_ErrUndefined()
+    {
+        var source = @"
+            :sub(Str1,2,5)→Str2
+            ";
+        AssertInterpretThrows<RuntimeError>(source, "ERR:UNDEFINED");
+    }
+
+    [TestMethod]
+    public void TestSub_NumberConstant()
+    {
+        var source = @"
+            :sub(550)→A
+            ";
+        Interpret(source, out _, out var environment);
+        Assert.AreEqual((TiNumber)5.5f, environment.Get<TiNumber>("A"));
+    }
+
+    [TestMethod]
+    public void TestSub_Number()
+    {
+        var source = @"
+            :125→A
+            :sub(A)→B
+            ";
+        Interpret(source, out _, out var environment);
+        Assert.AreEqual((TiNumber)1.25f, environment.Get<TiNumber>("B"));
+    }
+
+    [TestMethod]
+    public void TestSub_ErrArgument()
+    {
+        var source = @"
+            :sub(550,5)→A
+            ";
+        AssertInterpretThrows<RuntimeError>(source, "ERR:ARGUMENT");
+    }
+}
