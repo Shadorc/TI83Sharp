6b38b9127e336cf7f559c49c31d0a2cdc53d0e0b
Shadorc <d.ceccarelli@outlook.fr>
2025-04-09 00:49:05 +0200

    Add inString( command

---
+++ TI83Sharp/Memory/Callable/Function/Variable/String/InString.cs
@@ -0,0 +1,43 @@
+﻿
+namespace TI83Sharp;
+
+public class InString : Function
+{
+    public InString() : base("inString(", 2, 3)
+    {
+    }
+
+    public override object Call(Interpreter interpreter, List<Expr> arguments)
+    {
+        var arg1 = interpreter.Evaluate(arguments[0]);
+        if (arg1 is not string haystack)
+        {
+            throw RuntimeError.DataType;
+        }
+
+        var arg2 = interpreter.Evaluate(arguments[1]);
+        if (arg2 is not string needle)
+        {
+            throw RuntimeError.DataType;
+        }
+
+        var start = (TiNumber)1;
+        if (arguments.Count == 3)
+        {
+            var arg3 = interpreter.Evaluate(arguments[2]);
+            if (arg3 is not TiNumber startArg)
+            {
+                throw RuntimeError.DataType;
+            }
+
+            if (!startArg.IsInt() || startArg < 1)
+            {
+                throw RuntimeError.Domain;
+            }
+
+            start = startArg;
+        }
+
+        return (TiNumber)(haystack.IndexOf(needle, start - 1) + 1);
+    }
+}
---
+++ TI83SharpTests/MemoryTests/CallableTests/FunctionTests/Variable/StringTests/InStringTests.cs
@@ -0,0 +1,53 @@
+﻿using TI83Sharp;
+using static TI83SharpTests.TestsHelper;
+
+namespace TI83SharpTests;
+
+[TestClass]
+public class InStringTests
+{
+    [TestMethod]
+    public void TestInString()
+    {
+        var source = @"
+            :inString(""TI-BASIC"",""BASIC→A
+            ";
+        Interpret(source, out _, out var environment);
+        Assert.AreEqual((TiNumber)4, environment.Get<TiNumber>("A"));
+    }
+
+    [TestMethod]
+    public void TestInString_NotFound()
+    {
+        var source = @"
+            :inString(""TI-BASIC"",""ELEPHANT→A
+            ";
+        Interpret(source, out _, out var environment);
+        Assert.AreEqual((TiNumber)0, environment.Get<TiNumber>("A"));
+    }
+
+    [TestMethod]
+    public void TestInString_Start()
+    {
+        var source = @"
+            :inString(""TI-BASIC"",""I"",2→A
+            ";
+        Interpret(source, out _, out var environment);
+        Assert.AreEqual((TiNumber)2, environment.Get<TiNumber>("A"));
+
+        source = @"
+            :inString(""TI-BASIC"",""I"",3→A
+            ";
+        Interpret(source, out _, out environment);
+        Assert.AreEqual((TiNumber)7, environment.Get<TiNumber>("A"));
+    }
+
+    [TestMethod]
+    public void TestInString_ErrDomain()
+    {
+        var source = @"
+            :inString(""TI-BASIC"",""BASIC"",1.2→A
+            ";
+        AssertInterpretThrows<RuntimeError>(source, "ERR:DOMAIN");
+    }
+}
