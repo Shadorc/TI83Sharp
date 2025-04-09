e530b780de329c7336dc8cedc6b957c680fbfa9a
Shadorc <d.ceccarelli@outlook.fr>
2025-04-09 21:56:10 +0200

    Add tests for ³ and ²

---
+++ TI83SharpTests/MemoryTests/CallableTests/FunctionTests/MathTests/CubeTests.cs
@@ -0,0 +1,34 @@
+﻿using TI83Sharp;
+using static TI83SharpTests.TestsHelper;
+
+namespace TI83SharpTests;
+
+[TestClass]
+public class CubeTests
+{
+    [TestMethod]
+    public void TestCube()
+    {
+        var source =
+            @"
+            :5.32³→A
+            ";
+        Interpret(source, out _, out var environment);
+        Assert.AreEqual((TiNumber)150.56879f, environment.Get<TiNumber>("A"));
+
+        source =
+            @"
+            :4³→A
+            ";
+        Interpret(source, out _, out environment);
+        Assert.AreEqual((TiNumber)64, environment.Get<TiNumber>("A"));
+
+        source =
+            @"
+            :5→A
+            :A³→A
+            ";
+        Interpret(source, out _, out environment);
+        Assert.AreEqual((TiNumber)125, environment.Get<TiNumber>("A"));
+    }
+}
---
+++ TI83SharpTests/MemoryTests/CallableTests/FunctionTests/MathTests/SquareTests.cs
@@ -0,0 +1,34 @@
+﻿using TI83Sharp;
+using static TI83SharpTests.TestsHelper;
+
+namespace TI83SharpTests;
+
+[TestClass]
+public class SquareTests
+{
+    [TestMethod]
+    public void TestSquare()
+    {
+        var source =
+            @"
+            :5.32²→A
+            ";
+        Interpret(source, out _, out var environment);
+        Assert.AreEqual((TiNumber)28.302402f, environment.Get<TiNumber>("A"));
+
+        source =
+            @"
+            :4²→A
+            ";
+        Interpret(source, out _, out environment);
+        Assert.AreEqual((TiNumber)16, environment.Get<TiNumber>("A"));
+
+        source =
+            @"
+            :5→A
+            :A²→A
+            ";
+        Interpret(source, out _, out environment);
+        Assert.AreEqual((TiNumber)25, environment.Get<TiNumber>("A"));
+    }
+}
