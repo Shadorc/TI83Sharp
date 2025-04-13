6f0c0112a5d0cca089294f81e878c7977dd40966
Shadorc <d.ceccarelli@outlook.fr>
2025-04-13 18:57:21 +0200

    NoGUI: Refresh at 0.5Hz only when needed

--- TI83Sharp/IO/Output/ConsoleOutput.cs
+++ TI83Sharp/IO/Output/ConsoleOutput.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.InteropServices;
+using Timer = System.Threading.Timer;
 
 namespace TI83Sharp;
 
@@ -8,24 +9,48 @@ public class ConsoleOutput
     [return: MarshalAs(UnmanagedType.Bool)]
     private static extern bool AllocConsole();
 
+    private readonly Timer _refreshTimer;
+
+    private bool _needsRefresh;
+    private TiHomeScreen? _screen;
+
     public ConsoleOutput()
     {
         AllocConsole();
+
+        _refreshTimer = new Timer(new TimerCallback(_ => OutputScreen()), null, 0, 500);
+    }
+
+    ~ConsoleOutput()
+    {
+        _refreshTimer.Dispose();
     }
 
     public void OnScreenChange(object? sender, ScreenChangedEventArgs e)
     {
+        _screen = e.Screen;
+        _needsRefresh = true;
+    }
+
+    private void OutputScreen()
+    {
+        if (_screen == null || !_needsRefresh)
+        {
+            return;
+        }
+
         Console.Clear();
 
-        var screen = e.Screen;
         for (int y = 1; y < TiHomeScreen.HEIGHT + 1; y++)
         {
             for (int x = 1; x < TiHomeScreen.WIDTH + 1; x++)
             {
-                Console.Write(screen[x, y]);
+                Console.Write(_screen[x, y]);
             }
             Console.WriteLine();
         }
+
+        _needsRefresh = false;
     }
 
     public static void ShowErrorBeforeExit(string message)
